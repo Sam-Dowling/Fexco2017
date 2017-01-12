e8505c22ce2c15398f93d35542a180804f64231c
Sam Dowling <sam.dowling@students.ittralee.ie>
2017-01-12 22:12:17 +0000

    Added template methods for Probability Checker.

---
+++ LoanApp/src/ProbabilityChecker.java
@@ -0,0 +1,25 @@
+
+public class ProbabilityChecker {
+
+    private void TotalCost(){
+
+    }
+
+    private void BalanceScore(float balance){
+
+    }
+
+    private void EmploymentScore(boolean Employment){
+
+    }
+
+    private void CreditHistoryScore(float CreditHistory){
+
+    }
+
+    public int getScore(){
+        return 0;
+    }
+
+
+}
