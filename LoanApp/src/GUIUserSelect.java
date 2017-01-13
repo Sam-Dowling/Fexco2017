e240ea84a91283dbc9bd078a397abffe69e9cc07
LukeBluett <LukeBluett@mail.com>
2017-01-13 14:50:55 +0000

    new file:   LoanApp/src/GUIUserSelect.java
    Added file for the GUI that allows which user to login as.
    Added Comments and empty Methods
    This file is a singleton to allow only a single instance to be created, preventing issues with interacting with this GUI from other classes.

---
+++ LoanApp/src/GUIUserSelect.java
@@ -0,0 +1,61 @@
+/*
+ *
+ *	This Class creates a GUI for selecting what user to log in as.
+ *	It is a singleton to allow its controls to be accessed from other classes.
+ *	Components:
+ *			JLabel : labelUserSelect
+ *			JComboBox : comboBoxUserSelect
+ *			JButton : buttonUserSelectOK
+ *			JButton : buttonUserSelectClose
+ */
+
+public class GUIUserSelect {
+
+	// This creates a single instance of GUIUserSelect class
+	private static GUIUserSelect gui = new GUIUserSelect();
+
+	// This Constructor is private to prevent another instance of this class to be created
+	private GUIUserSelect() {
+		createComponents();
+		addComponents();
+		addActionListeners();
+	}
+
+	// This returns the only instance of this class to be created
+	public static GUIUserSelect getInstance() { return gui; }
+
+	/*
+	   Initialize Components and give their default values
+	*/
+	private void createComponents() {
+	
+	}
+
+	/*
+	   Add Components to the correct layouts and add the layouts to the Frame
+	*/
+	private void addComponents() {
+	
+	}
+
+	/*
+	   Add the ActionListeners for OK Button and the Close Button
+	*/
+	private void addActionListeners() {
+	
+	}
+
+	/*
+	   Allow other classes to open this gui
+	*/
+	public static void setFrameVisible() {
+	
+	}
+
+	/*
+	   Allows other classes to close this gui
+	*/
+	public static void closeFrame() {
+	
+	}
+}
